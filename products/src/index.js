0316b43f904472a321ab8e803029d1f5ac60b500
David Barcenas <daveepro@outlook.com>
2022-04-15 12:56:34 -0500

    implement module federation

--- products/src/index.js
+++ products/src/index.js
@@ -7,4 +7,4 @@ for (let i = 0; i < 10; i++) {
   products += `<div><h3>${name}</h3></div>`
 }
 
-document.getElementById('root').innerHTML = products
+document.getElementById('root-products').innerHTML = products
