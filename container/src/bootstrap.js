cf615958c659690fb44c1bcb108f0470abfa800f
David Barcenas <daveepro@outlook.com>
2022-04-19 16:23:35 -0500

    init microfrontend with frameworks

--- container/src/bootstrap.js
+++
@@ -1,5 +0,0 @@
-import { mount as productsMount } from 'products/ProductsIndex'
-import { mount as cartMount } from 'cart/CartShow'
-
-productsMount(document.getElementById('my-products'))
-cartMount(document.getElementById('my-cart'))
