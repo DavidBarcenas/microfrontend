c4a2dab969cb1d05886f27ac67bf5d3c065a7fca
David Barcenas <daveepro@outlook.com>
2022-04-19 17:23:09 -0500

    setup react in landing module

---
+++ landing/src/bootstrap.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+
+// Mount function to start up the app
+const mount = (el) => {
+  const root = createRoot(el);
+  root.render(<h1>Hello Landing</h1>);
+}
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = mount(document.getElementById('_landing-root'))
+
+  if (devRoot) {
+    mount(devRoot)
+  }
+}
+
+// We are running through container
+// and we should export the mount function
+export { mount }
