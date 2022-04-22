54abbf6bd4caf265dbdded1986f0e22fd2deeeb9
David Barcenas <daveepro@outlook.com>
2022-04-22 17:57:16 -0500

    setup dashboard project

---
+++ dashboard/src/bootstrap.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app';
+
+// Mount function to start up the app
+const mount = (el) => {
+  if(el) {
+    const root = createRoot(el);
+    root.render(<App />);
+  }
+}
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = mount(document.getElementById('_dashboard-root'))
+
+  if (devRoot) {
+    mount(devRoot)
+  }
+}
+
+// We are running through container
+// and we should export the mount function
+export { mount }
