bbb983832336b371a2ecc5aae6d74f58ad9594b6
David Barcenas <daveepro@outlook.com>
2022-04-22 12:33:42 -0500

    add public path to webpack.dev

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signin from './components/signin';
+import Signup from './components/signup';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Auth</h1>} />
+        <Route path="/auth/signin" element={<Signin />} />
+        <Route path="/auth/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   )
